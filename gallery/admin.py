6375fe0a8898fa1881c32011e6e8df22c4b4710d
Bryson69 <brysonmundia@gmail.com>
2020-09-21 21:52:09 +0300

    Register models in order to appear on admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Gallery, categories, location
 
 # Register your models here.
+
+admin.site.register(Gallery)
+admin.site.register(categories)
+admin.site.register(location)
