bddc21caa71d4696e9819c89378d4cd76e52f6d3
Bryson69 <brysonmundia@gmail.com>
2020-10-01 18:34:06 +0300

    Changed contents of the database

--- dgv1/settings.py
+++ dgv1/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'dgv1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vault',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 
